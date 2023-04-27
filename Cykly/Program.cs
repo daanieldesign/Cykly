c7bf3c4cd30ab98fb4ea22b9164d55d3c5a13e36
daanieldesign <mahdakdaniel@gmail.com>
2023-04-27 13:16:46 +0200

    update

--- Cykly/Program.cs
+++ Cykly/Program.cs
@@ -10,10 +10,15 @@ namespace Cykly
     {
         static void Main(string[] args)
         {
+            /*
             //Cyklus s nekonečným počtem opakování
             //Cyklus s podmínkou na začátku, pokud není splněna podmínka, tak se ani nespustí
             int counter = 0;
-            while (true) //Vyhodnocování podmínky
+
+            Console.WriteLine("Chcete zapnout nekonečný cyklus? True/False");
+            bool starter = bool.Parse(Console.ReadLine());
+
+            while (starter) //Vyhodnocování podmínky
             {
                 //Příkaz
                 counter = counter + 1;
@@ -21,6 +26,20 @@ namespace Cykly
             }
 
             Console.WriteLine("Podmínka nebyla splněna a dostal jsem se tady!");
+            */
+
+            //Cyklus s pevným počtem opakování
+            //for(počáteční hodnota, podmínka, příkaz)
+            //i++ –> inkrementace, přičtení o jedna
+            //i-- –> dekrementace, snížení o jedna
+            Console.WriteLine("Napište počet opakování:");
+            int x = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadKey();
         }
     }
