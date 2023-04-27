4eab0a49d5933586600ac5bb9fab88ddeff31222
daanieldesign <mahdakdaniel@gmail.com>
2023-04-27 12:15:01 +0200

    update

--- Cykly/Program.cs
+++ Cykly/Program.cs
@@ -10,7 +10,18 @@ namespace Cykly
     {
         static void Main(string[] args)
         {
+            //Cyklus s nekonečným počtem opakování
+            //Cyklus s podmínkou na začátku, pokud není splněna podmínka, tak se ani nespustí
+            int counter = 0;
+            while (true) //Vyhodnocování podmínky
+            {
+                //Příkaz
+                counter = counter + 1;
+                Console.WriteLine(counter);
+            }
 
+            Console.WriteLine("Podmínka nebyla splněna a dostal jsem se tady!");
+            Console.ReadKey();
         }
     }
 }
